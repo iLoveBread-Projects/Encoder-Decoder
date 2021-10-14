3d1bff73c825cf9f5348e15b9fde8ffd66f474fc
error404-69-dotcom <robocookieofficial@gmail.com>
2021-10-14 19:55:15 +0200

    init

---
+++ EncoderDecoder/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace EncoderDecoder
+{
+    partial class frmDEcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(16, 51);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(107, 23);
+            this.btnEncode.TabIndex = 0;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(16, 25);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(219, 20);
+            this.txtText.TabIndex = 2;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(128, 51);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(107, 23);
+            this.btnDecode.TabIndex = 3;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(13, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(52, 13);
+            this.lblInfo.TabIndex = 4;
+            this.lblInfo.Text = "Text here";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(16, 80);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(219, 204);
+            this.txtResult.TabIndex = 5;
+            // 
+            // frmDEcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 297);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnEncode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frmDEcode";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encoder and Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ EncoderDecoder/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncoderDecoder
+{
+    public partial class frmDEcode : Form
+    {
+        public frmDEcode()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+        }
+        public static string EncodeServerName(string serverName)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(serverName));
+        }
+        public static string DecodeServerName(string encodedServername)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(encodedServername));
+        }
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string encode = EncodeServerName(txtText.Text);
+
+                txtResult.Text = encode;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not encode the message.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string decode = DecodeServerName(txtText.Text);
+
+                txtResult.Text = decode;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not decode the message.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
